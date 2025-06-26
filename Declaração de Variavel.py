15985e6e28707c60ca884fe48977e226e54af441
Vinicius Chinaqui <167335156+Chinaqui@users.noreply.github.com>
2025-06-26 08:52:01 -0300

    Add files via upload

---
+++ Declaração de Variavel.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o código do funcionario "))
+nome = input("Digite o nome do funcionario: ")
+salario = float(input("Digite o salario do funcionario: "))
+ativo = True
+
+print("Código: %d" % codigo)
+print("Nome: %s" % nome)
+print("Salario: %.2f" % salario)
+print("Ativo: %r" % ativo)
